f61f41c75cd369cc178a22d6e10c4761b0cc0a1c
yxm <yinxiaoming96@gmail.com>
2017-03-19 00:49:45 +0800

    .

---
+++ sc-main/src/main/java/com/studycandy/conf/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.studycandy.conf;
+
+import com.studycandy.interceptor.CoreInterceptor;
+import flybear.hziee.core.base.BaseInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new BaseInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(new CoreInterceptor()).addPathPatterns("/**").excludePathPatterns("/public");
+        super.addInterceptors(registry);
+    }
+}
---
+++ sc-main/src/main/java/com/studycandy/core/BaseController.java
@@ -0,0 +1,12 @@
+package com.studycandy.core;
+
+/**
+ * Coding with Intellij IDEA
+ * Author: Chenls
+ * Time: 2017/3/18
+ */
+public class BaseController extends flybear.hziee.core.base.BaseController {
+    public BaseController() {
+
+    }
+}
