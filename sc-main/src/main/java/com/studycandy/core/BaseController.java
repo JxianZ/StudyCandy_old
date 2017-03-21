c43db541e3ec7c3fd64848090442419e6b702d54
Chenls <chenls1997@gmail.com>
2017-03-21 20:25:11 +0800

    Update 2017年3月21日20:25:06

--- sc-main/src/main/java/com/studycandy/core/BaseController.java
+++ sc-main/src/main/java/com/studycandy/core/BaseController.java
@@ -1,12 +1,30 @@
 package com.studycandy.core;
 
+import com.studycandy.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpSession;
+
+import static com.studycandy.constant.Constant.SESSION_CURRENT_USER;
+
 /**
  * Coding with Intellij IDEA
  * Author: Chenls
  * Time: 2017/3/18
  */
 public class BaseController extends flybear.hziee.core.base.BaseController {
-    public BaseController() {
+    @Autowired
+    private HttpSession httpSession;
 
+    public BaseController() {
+    }
+
+    protected HttpSession getHttpSession() {
+        return httpSession;
+    }
+
+    protected User getCurrentUser() {
+        User cu = (User) httpSession.getAttribute(SESSION_CURRENT_USER);
+        return cu;
     }
 }
