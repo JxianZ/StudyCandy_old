5afade21937dc7e4ff8731ab03139f09812f5458
5rKB5bPlusD <x1654102203@gmail.com>
2017-04-09 01:05:23 +0800

    Merge remote-tracking branch 'origin/zxk'
    # Conflicts:
    #	src/main/main.iml
    #	web-ssm.iml

--- sc-main/src/main/webapp/static/js/post.js
+++ sc-main/src/main/webapp/static/js/post.js
@@ -2,16 +2,12 @@
  * Created by BlackZXK on 4/5/2017.
  */
 $(function() {
-    //判断当前的登录用户是不是文章的作者
-    if ("username" == "username") {
-        $(".mybtn2").toggle();
-    }
 
     $(".mybtn2").click(function() {
         $("#postContent").val($("#describe").html());
     });
 
-    $(".mybtn").click(function() {
+    $(".mybtn1").click(function() {
         $("#postContent").val('');
     });
 
